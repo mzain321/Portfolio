b2e4232025bfbe38a81b04d290783b4b84696f09
Muhammad Zain <zain0909012@gmail.com>
2022-08-11 12:20:35 +0500

    Update settings.py

--- myweb/settings.py
+++ myweb/settings.py
@@ -127,7 +127,7 @@ STATICFILES_DIRS=[
 os.path.join(BASE_DIR,'staticfiles')
 ]
 
-MEDIA_ROOT=os.path.join(BASE_DIR,'static/images')
+MEDIA_ROOT=os.path.join(BASE_DIR,'staticfiles/images')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
